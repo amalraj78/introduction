dd87934c6a301df8601e1380e9fc2e1e0c142060
amalraj78 <amalraj.johncharlesvaratharajan@ensea.fr>
2024-09-20 09:41:04 +0200

    Message de commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//declarer variable
+let firstName = "Bob"
+let age = 14
+
+//changer de valeur
+firstName = "AMal"
+age = 22
+
+
+console.log(firstName)
+console.log(age)
+
+
+git config --global user.name "amalraj78"
+git config --global user.email "votre.email@example.com"
